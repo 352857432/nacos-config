cf8226d126d447bc5eb7a70bbb8176bd78e10e80
Edison <javafa@163.com>
2021-10-05 20:49:23 +0800

    自定义共享 Data Id 配置

--- service1/src/main/java/com/test/nacos/service1/GetCogfig.java
+++ service1/src/main/java/com/test/nacos/service1/GetCogfig.java
@@ -8,22 +8,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//1.自动刷新
+@RefreshScope
 public class GetCogfig {
 
     //通过value注解读取配置信息
     //nacos已经通知客户端，客户端不会更新
     @Value("${common.name}")
+
     private String name;
-
-    //会动态刷新
-    @Autowired
-    ConfigurableApplicationContext applicationContext;
-
     @GetMapping("/getConfig1")
     public  String getConfig1(){
         System.out.println(name);
         return name;
     }
+
+
+
+
+    //2.自动刷新
+    @Autowired
+    ConfigurableApplicationContext applicationContext;
+
     @GetMapping("/getConfig2")
     public  String getConfig2(){
         System.out.println(name);
